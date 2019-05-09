b4b4b96b6e1bf25d0d368e8db76bbbb05c9fd0f3
Simon Davis <simonfreelance1@gmail.com>
2019-05-08 21:30:05 -0400

    Initial push

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+//How CRA uses environment variables
+//https://facebook.github.io/create-react-app/docs/adding-custom-environment-variables
+const App: React.FC = () => {
+  return <div className="App">Hello world!</div>;
+};
+
+export default App;
