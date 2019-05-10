e6bc9c30a680db6b628b8da4073bd9f9685544f7
Simon Davis <simonfreelance1@gmail.com>
2019-05-10 16:52:09 -0400

    Registered web components and found solution for typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 //How CRA uses environment variables
 //https://facebook.github.io/create-react-app/docs/adding-custom-environment-variables
 const App: React.FC = () => {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="App">
+      <mi-selection-card />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/web-components.d.ts
@@ -0,0 +1,8 @@
+import { any } from "prop-types";
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            ["mi-selection-card"]: any
+        }
+    }
+}
