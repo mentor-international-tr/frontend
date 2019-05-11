e42dfb13ed240cef3fc90214dc13d244ecd1679a
Simon Davis <simonfreelance1@gmail.com>
2019-05-11 18:32:23 -0400

    Created create group page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { Home } from "./route-components/index";
+import {
+  Home,
+  CreateGroup,
+} from "./route-components/index";
 
 //How CRA uses environment variables
 //https://facebook.github.io/create-react-app/docs/adding-custom-environment-variables
@@ -8,6 +11,11 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <Route exact path="/" component={Home} />
+      <Route
+        exact
+        path="/create-group"
+        component={CreateGroup}
+      />
     </div>
   );
 };
--- src/route-components/index.ts
+++ src/route-components/index.ts
@@ -1,1 +1,4 @@
 export { default as Home } from "./home/Home";
+export {
+  default as CreateGroup,
+} from "./create-group/CreateGroup";
