4c1bbb2eabaab22b7e2cab109d1cd8446a4b7c87
Simon Davis <simonfreelance1@gmail.com>
2019-05-10 20:13:04 -0400

    Started getting cards in each row in home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import "./home.scss";
+import { selectionCardData } from "./homeData/index";
+import {
+  itemsPerRow,
+  cloneObjectWithKeyID,
+} from "../../lib/helpers/index";
 export interface HomeProps {}
 
 const Home: React.SFC<HomeProps> = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <section className="selection-cards">
+        {itemsPerRow(
+          selectionCardData.map(card => {
+            return cloneObjectWithKeyID(card);
+          }),
+          2,
+        ).map(row => {
+          return (
+            <div className="selection-cards__row">
+              {row.map(card => {
+                return (
+                  <mi-selection-card
+                    key={card.keyID}
+                    description={card.description}
+                    extra-info={
+                      card.extraInfo ? card.extraInfo : null
+                    }
+                  >
+                    <i
+                      className={card.iconClassName}
+                      slot="icon"
+                    />
+                  </mi-selection-card>
+                );
+              })}
+            </div>
+          );
+        })}
+
+        {/* client, country manager, board member */}
+      </section>
+    </div>
+  );
 };
 
 export default Home;
