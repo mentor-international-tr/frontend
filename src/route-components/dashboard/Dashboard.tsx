64d1367aa9cbb9aa6b57a94b18659d619d5ac774
Simon Davis <simonfreelance1@gmail.com>
2019-05-14 01:24:53 -0400

    Basic Dashboard Header completed

--- src/route-components/dashboard/Dashboard.tsx
+++ src/route-components/dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import "./dashboard.scss";
+import React, { useState, useEffect } from "react";
 import DashSidebar from "./components/DashSidebar/DashSidebar";
 import StarterContent from "./components/StartingContent/StarterContent";
 import MainContent from "./components/MainContent/MainContent";
@@ -6,7 +7,10 @@ import MainContent from "./components/MainContent/MainContent";
 export interface DashboardProps {}
 
 const Dashboard: React.SFC<DashboardProps> = () => {
-  const [hasFirstGroup, setHasFirstGroup] = useState(true);
+  const [hasFirstGroup, setHasFirstGroup] = useState(false);
+  useEffect(() => {
+    setHasFirstGroup(true);
+  }, []);
   return (
     <div className="dashboard-container">
       <DashSidebar />
--- src/route-components/dashboard/components/MainContent/MainContent.tsx
+++ src/route-components/dashboard/components/MainContent/MainContent.tsx
@@ -1,10 +1,56 @@
 import "./main-content.scss";
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "components/Navbar/Navbar";
+import { Avatar, Divider } from "@material-ui/core";
+import PrimaryButton from "components/PrimaryButton/PrimaryButton";
 
 export interface MainContentProps {}
 
 const MainContent: React.SFC<MainContentProps> = () => {
-  return <div>main content</div>;
+  const MESSAGES = "Messages";
+  const TEMPLATES = "Templates";
+  const PEOPLE = "People";
+  const [activeNavText, setActiveNavText] = useState(
+    MESSAGES,
+  );
+  console.log(activeNavText);
+  return (
+    <div className="dashboard-main-content">
+      <header className="dashboard-header">
+        <Avatar className="dashboard-header__avatar">
+          s
+        </Avatar>
+        <div className="dashboard-header__text-content">
+          <h2>Training Cohort #1</h2>
+          <p>@peru1</p>
+        </div>
+        <div className="dashboard-header__buttons">
+          <button className="dashboard-header__btn-contacts">
+            Add Contacts
+          </button>
+          <button className="dashboard-header__btn-settings">
+            <i className="fas fa-cog" />
+          </button>
+        </div>
+      </header>
+      <Navbar
+        setText={setActiveNavText}
+        navItems={[
+          {
+            text: MESSAGES,
+          },
+          {
+            text: TEMPLATES,
+          },
+          {
+            text: PEOPLE,
+          },
+        ]}
+      />
+      <Divider />
+      <main />
+    </div>
+  );
 };
 
 export default MainContent;
