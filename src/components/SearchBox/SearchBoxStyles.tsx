901da95a88a45e162292c46db9797dbfc4a54087
Simon Davis <simonfreelance1@gmail.com>
2019-05-15 18:11:32 -0400

    Fixed google dev tools console error

--- src/components/SearchBox/SearchBoxStyles.tsx
+++ src/components/SearchBox/SearchBoxStyles.tsx
@@ -2,19 +2,21 @@ import styled from "styled-components";
 import vars from "lib/styles/variables";
 import {
   InputBase as MaterialInputBase,
-  Paper as MaterialPaper,
+  Paper,
 } from "@material-ui/core";
 
-const Paper = styled(MaterialPaper)`
+const MIPaper = styled(Paper)`
   display: flex;
   justify-content: center;
   max-width: 600px;
-  border: ${(props: { isMicOn: boolean }) =>
-    props.isMicOn ? `2px solid ${vars.light_blue}` : ""};
+  border: ${(props: { ismicon: string }) =>
+    props.ismicon === "true"
+      ? `2px solid ${vars.light_blue}`
+      : ""};
 `;
 
 const InputBase = styled(MaterialInputBase)`
   width: 100%;
 `;
 
-export { Paper, InputBase };
+export { MIPaper, InputBase };
