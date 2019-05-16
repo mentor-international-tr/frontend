0471b1e6453664206ee3b5fd68237b89477a94ba
Simon Davis <simonfreelance1@gmail.com>
2019-05-16 19:27:54 -0400

    Finished Layout of PeopleSection

--- src/route-components/dashboard/components/PeopleSection/PeopleSection.tsx
+++ src/route-components/dashboard/components/PeopleSection/PeopleSection.tsx
@@ -2,21 +2,26 @@ import "./people-section.scss";
 import React from "react";
 import SearchBox from "components/SearchBox/SearchBox";
 import MITable from "components/MITable/MITable";
+import { Button } from "@material-ui/core";
 
 export interface PeopleSectionProps {}
 
 const PeopleSection: React.SFC<PeopleSectionProps> = () => {
   return (
     <section className="people-section">
-      <SearchBox
-        searchText={""}
-        setSearchText={() => {}}
-        placeholderWhileMicOn={
-          "Tell me who you want to find."
-        }
-        placeholder={"Search for people"}
-      />
-
+      <header className="people-section__header">
+        <SearchBox
+          searchText={""}
+          setSearchText={() => {}}
+          placeholderWhileMicOn={
+            "Tell me who you want to find."
+          }
+          placeholder={"Search for people"}
+        />
+        <Button variant="contained" color="primary">
+          Add People
+        </Button>
+      </header>
       <MITable
         className="people-joined-table"
         tableHeaders={["Name", "Date Joined"]}
